fa1a124139c512af46f09eb3139e9f81e60392d1
rikelmerodriguez <rodriguezrikelme17@gmail.com>
2024-07-20 20:58:27 -0300

    list_cypress

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  e2e: {
+    baseUrl: 'https://agenda-contatos-react.vercel.app',
+    setupNodeEvents(on, config) {
+      // Implemente os event listeners aqui, se necessário
+    },
+  },
+};
